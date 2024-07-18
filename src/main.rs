c0968e8f69bef09865c8097f0d28769e5b31551c
Sadagat Asgarov <67765925+sadagatasgarov@users.noreply.github.com>
2024-07-18 14:33:00 +0000

    uniq deger

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,16 @@
-
 fn main() {
-    let list = vec![1.0, 5.0, 6.0,7.0];
-    println!("{:.1}", median_calc(list).unwrap())
+    let list = vec![1, 4, 4, 55, 55, 32, 1];
+    let a = uniq(list);
+    println!("{a:?}")
 }
 
-fn median_calc(mut vec: Vec<f32>) -> Option<f32> {
-    if vec.is_empty() {
-        return None;
+fn uniq<T: PartialEq>(vec: Vec<T>) -> Vec<T> {
+    let mut tmp: Vec<T> = Vec::new();
+    for el in vec {
+        if tmp.contains(&el) {
+            continue;
+        }
+        tmp.push(el);
     }
-
-    vec.sort_by(|a, b| a.partial_cmp(b).unwrap());
-
-    let n_elements = vec.len();
-    let middle: usize = n_elements / 2;
-    let vec_is_even = n_elements % 2 == 0;
-    let med  =  if vec_is_even {
-        // mean  / average of the middle-two elements
-        Some((vec[middle] + vec[middle - 1]) / 2.0)
-    } else {
-        Some(vec[middle])
-    };
-
-    med
+    tmp
 }
